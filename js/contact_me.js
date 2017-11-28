95a15e94edef1eb002b78cc4ea54e6d8e606abe1
Zoran Prnjak <zoran.prnjak@gmail.com>
2017-11-29 00:40:10 +0100

    contact form

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,34 +1,16 @@
-$(function() {
-
+$(function () {
     $("input,textarea").jqBootstrapValidation({
         preventSubmit: true,
-        submitError: function($form, event, errors) {
-            // additional error messages or events
-        },
-        submitSuccess: function($form, event) {
-            event.preventDefault(); // prevent default submit behaviour
-            // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
-            $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
-                cache: false,
-                success: function() {
-                    // Success message
+        submitSuccess: function ($form, event) {
+            event.preventDefault(); 
+            var data = getFormData();
+
+            var url = event.target.action;
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', url);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.onreadystatechange =
+                function () {
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
@@ -36,35 +18,61 @@ $(function() {
                         .append("<strong>Your message has been sent. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
-
-                    //clear all fields
                     $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            })
+                    return false;
+                };
+            var encoded = Object.keys(data).map(function (k) {
+                return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+            }).join('&')
+            xhr.send(encoded);
         },
-        filter: function() {
+        filter: function () {
             return $(this).is(":visible");
         },
     });
 
-    $("a[data-toggle=\"tab\"]").click(function(e) {
+    $("a[data-toggle=\"tab\"]").click(function (e) {
         e.preventDefault();
         $(this).tab("show");
     });
 });
 
-
-/*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
+$('#name').focus(function () {
     $('#success').html('');
 });
+
+function getFormData() {
+    var form = document.getElementById("contactForm");
+    var elements = form.elements; 
+    var fields = Object.keys(elements).map(function (k) {
+        if (elements[k].name !== undefined) {
+            return elements[k].name;
+        } else if (elements[k].length > 0) {
+            return elements[k].item(0).name;
+        }
+    }).filter(function (item, pos, self) {
+        return self.indexOf(item) == pos && item;
+    });
+    var data = {};
+    fields.forEach(function (k) {
+        data[k] = elements[k].value;
+        var str = "";
+        if (elements[k].type === "checkbox") {
+            str = str + elements[k].checked + ", ";
+            data[k] = str.slice(0, -2); 
+        } else if (elements[k].length) {
+            for (var i = 0; i < elements[k].length; i++) {
+                if (elements[k].item(i).checked) {
+                    str = str + elements[k].item(i).value + ", ";
+                    data[k] = str.slice(0, -2);
+                }
+            }
+        }
+    });
+
+    data.formDataNameOrder = JSON.stringify(fields);
+    data.formGoogleSheetName = form.dataset.sheet || "responses";
+    data.formGoogleSendEmail = form.dataset.email || "";
+
+    return data;
+}
